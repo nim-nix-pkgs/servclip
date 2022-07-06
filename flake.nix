026a795624eac93888c8faa0b122ab230c194ac9
hugosenari <hugosenari@gmail.com>
2022-07-06 20:35:30 -0300

    chore: re index servclip

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."servclip-master".dir   = "master";
+  inputs."servclip-master".owner = "nim-nix-pkgs";
+  inputs."servclip-master".ref   = "master";
+  inputs."servclip-master".repo  = "servclip";
+  inputs."servclip-master".type  = "github";
+  inputs."servclip-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."servclip-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
